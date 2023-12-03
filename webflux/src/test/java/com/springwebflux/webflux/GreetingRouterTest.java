823d4702b205acc30d729e4345eb161a80b13a8f
Subij Shrestha <36372614+subijstha@users.noreply.github.com>
2023-12-02 19:08:00 -0700

    test added

--- webflux/src/test/java/com/springwebflux/webflux/GreetingRouterTest.java
+++ webflux/src/test/java/com/springwebflux/webflux/GreetingRouterTest.java
@@ -1,13 +1,35 @@
 package com.springwebflux.webflux;
 
+import com.springwebflux.webflux.domain.Greeting;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+import org.springframework.test.web.reactive.server.WebTestClient;
 
-@SpringBootTest
-class WebfluxApplicationTests {
+import static org.assertj.core.api.Assertions.assertThat;
+
+@ExtendWith(SpringExtension.class)
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+public class GreetingRouterTest {
+
+	@Autowired
+	private WebTestClient webTestClient;
 
 	@Test
-	void contextLoads() {
+	public void testHello() {
+		webTestClient
+				// Create a GET request to test an endpoint
+				.get().uri("/hello")
+				.accept(MediaType.APPLICATION_JSON)
+				.exchange()
+				// and use the dedicated DSL to test assertions against the response
+				.expectStatus().isOk()
+				.expectBody(Greeting.class).value(greeting -> {
+					assertThat(greeting.getMessage()).isEqualTo("Hello, World!");
+				});
 	}
-
 }
