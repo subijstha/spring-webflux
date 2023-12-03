696e479b8bf2134a24d94237eebc8e07c842d435
Subij Shrestha <36372614+subijstha@users.noreply.github.com>
2023-12-02 18:30:56 -0700

    Greeting handler done

---
+++ webflux/src/main/java/com/springwebflux/webflux/handler/GreetingHandler.java
@@ -0,0 +1,17 @@
+package com.springwebflux.webflux.handler;
+
+import com.springwebflux.webflux.domain.Greeting;
+import org.springframework.http.MediaType;
+import org.springframework.stereotype.Component;
+import org.springframework.web.reactive.function.BodyInserters;
+import org.springframework.web.reactive.function.server.ServerRequest;
+import org.springframework.web.reactive.function.server.ServerResponse;
+import reactor.core.publisher.Mono;
+
+@Component
+public class GreetingHandler {
+    public Mono<ServerResponse> hello(ServerRequest serverRequest){
+        return ServerResponse.ok().contentType(MediaType.APPLICATION_JSON)
+                .body(BodyInserters.fromValue(new Greeting("hello SPring")));
+    }
+}
